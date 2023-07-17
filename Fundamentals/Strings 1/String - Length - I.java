19362b097de8d42a3b252eea6705613750c02246
Anuja M <124621141+Anuja-M123@users.noreply.github.com>
2023-07-17 12:50:08 +0530

    Create String - Length - I.java

---
+++ Fundamentals/Strings 1/String - Length - I.java
@@ -0,0 +1,20 @@
+/* Write a  program to find the length of the given string using string library functions:
+
+Input Format:
+
+Input consist of 1 string.
+Sample Input & Output:
+
+Java
+
+The length of Java is 4*/
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+    Scanner sc=new Scanner(System.in);
+    String anu=sc.nextLine();
+    System.out.print("The length of "+anu +" is "+anu.length());
+    }
+}
